5d4173f9a90e9872b2a639ea79f80f6209f6db75
Mayar Osama <mayarosama18@gmail.com>
2022-04-04 09:00:04 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from configparser import ConfigParser
+
+config= ConfigParser()
+print(config.sections())
+
+config.read('configs.ini')
+print(config.sections())
+
+print(config['DEFAULT']['title'])
+print(config['database']['host'])
